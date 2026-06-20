2f8db544d109b36f3f52d1454b4701c2d397122f
BhavyaAudisri <somisetty.bhavya@gmail.com>
2026-06-20 18:13:35 +0530

    backend dockerfile updated

--- backend/app/server.js
+++ backend/app/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
